fcbea749b4ea404c38623b6982e54aa277bcbfba
alejandra cantu <alejandra.cantu13@gmail.com>
2018-11-07 03:50:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
   "Fizz"
-elseif int % 5 == 0
+elsif int % 5 == 0
   "Buzz"
-elseif int % 5 == 0 && int % 3 == 0
+elsif int % 5 == 0 && int % 3 == 0
   "Fizzbuzz"
 else "nill"
 end
