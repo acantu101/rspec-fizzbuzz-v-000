b3dd17cb48012a35d0e891b038b0df8b54b59ac2
alejandra cantu <alejandra.cantu13@gmail.com>
2018-11-07 03:51:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
+  elsif int % 5 == 0 && int % 3 == 0
+  "Fizzbuzz"
   if int % 3 == 0
   "Fizz"
 elsif int % 5 == 0
   "Buzz"
-elsif int % 5 == 0 && int % 3 == 0
-  "Fizzbuzz"
 else "nill"
 end
 end
